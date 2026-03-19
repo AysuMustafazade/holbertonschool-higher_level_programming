8281df2bfc513ed0d1b173021b49fe7b8029f551
AysuMustafazade <mustafazadays16@gmail.com>
2026-03-19 12:20:45 +0000

    retrieves an element

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -6,5 +6,4 @@ def element_at(my_list, idx):
     elif idx > (len(my_list)-1):
         return None
     else:
-        print(my_list[idx])
-    print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))        
+        print(my_list[idx])        
