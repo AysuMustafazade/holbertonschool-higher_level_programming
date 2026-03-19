99b504473c64100e56914402d136c677c24e3535
AysuMustafazade <mustafazadays16@gmail.com>
2026-03-19 10:02:15 +0000

    Add arguments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
         for i in range (1,len(sys.argv)):
             num = int(sys.argv[i])
             total = total + num
-            print(total)
+        print(total)
