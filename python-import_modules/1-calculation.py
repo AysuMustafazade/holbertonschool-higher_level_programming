8b1b85c0747b4474b9a6e8a767becd8cc5717999
AysuMustafazade <mustafazadays16@gmail.com>
2026-03-19 08:57:50 +0000

    Calculation

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -16,7 +16,9 @@ def div(a, b):
 
 a = 10
 b = 5
-print ("{} + {} = {}".format(a, b, add(a,b)))
-print ("{} - {} = {}".format(a, b, sub(a,b)))
-print ("{} * {} = {}".format(a, b, mul(a,b)))
-print ("{} / {} = {}".format(a, b, div(a, b)))
+if __name__ == "__main__":
+    print ("{} + {} = {}".format(a, b, add(a,b)))
+    print ("{} - {} = {}".format(a, b, sub(a,b)))
+    print ("{} * {} = {}".format(a, b, mul(a,b)))
+    print ("{} / {} = {}".format(a, b, div(a, b)))
+    
