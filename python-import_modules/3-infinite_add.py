338a49c8b2c8f4615c6c9c3f0f6405cfb70505e4
AysuMustafazade <mustafazadays16@gmail.com>
2026-03-19 10:02:47 +0000

    Add arguments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print(0)
     else:
-        for i in range (1,len(sys.argv)):
+        for i in range(1, len(sys.argv)):
             num = int(sys.argv[i])
             total = total + num
         print(total)
