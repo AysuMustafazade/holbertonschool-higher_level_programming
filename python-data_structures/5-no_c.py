d33b329e565e02886f8571de018b81f9b29fa4a7
AysuMustafazade <mustafazadays16@gmail.com>
2026-03-20 22:59:34 +0000

    Remove char

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,4 +4,5 @@ def no_c(my_string):
     char1 = "c"
     char2 = "C"
     new_string = ''.join(char for char in my_string if char != char1 or char != char2)
-    return my_string
+
+return my_string
