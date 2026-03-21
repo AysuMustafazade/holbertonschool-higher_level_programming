2e1b02a6bbd05da2502d7e830d6ff06ac9d1bc64
AysuMustafazade <mustafazadays16@gmail.com>
2026-03-21 20:30:30 +0000

    Multiple returns

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        sentence[0] = None
+        return (0, None)
     else:
-        return len(sentence), sentence[0]    
+        return (len(sentence), sentence[0])
     print("Length: {:d} - First character: {}".format(len(sentence), sentence[0]))
