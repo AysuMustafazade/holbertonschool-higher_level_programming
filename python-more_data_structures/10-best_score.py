07ca1fd9d9ea8cd1937e64917f58d0191b086e4c
AysuMustafazade <mustafazadays16@gmail.com>
2026-03-26 22:33:14 +0000

    Max of values

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
     best_score = max(a_dictionary.values())
-    return best_score
+    for key, value in a_dictionary.items():
+        if value == best_score:
+            return key
