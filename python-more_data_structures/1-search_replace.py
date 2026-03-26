b6e6348aef23c2a3552883f131ca604c83d4bf1b
AysuMustafazade <mustafazadays16@gmail.com>
2026-03-26 21:23:07 +0000

    Search and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    for i in my_list:
-        if i == search:
-            search = replace
-        return my_list    
+    for i in range (0, len(my_list)):
+        if my_list[i] == search:
+            my_list[i] = replace
+    return my_list    
