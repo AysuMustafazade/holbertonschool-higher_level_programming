1bac4e8202582a9065d31d06ef7c039d08b0684d
AysuMustafazade <mustafazadays16@gmail.com>
2026-03-26 22:08:24 +0000

    update dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    new_dict = a_dictionary.copy()
-    for key1 in new_dict:
-        if key1 == key:
-            return key1, value
-    print(f'{key}: {a_dictionary[key]} + " ---" + {key1}: {new_dict[key1]}')
+    a_dictionary[key] = value
+    return a_dictionary
