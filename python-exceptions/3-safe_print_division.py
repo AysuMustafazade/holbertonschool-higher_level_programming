3c8927ccf8f20c145a595e8a49a4c3873c46bd0f
AysuMustafazade <mustafazadays16@gmail.com>
2026-03-28 17:05:22 +0000

    Division

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -6,5 +6,4 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return None
     finally:
-        return result
-    print("Inside result: {}".format(result))
+        print("Inside result: {}".format(result))
