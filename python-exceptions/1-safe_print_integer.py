75d786b1999bf70576f30a82dfe3e651fabdd444
AysuMustafazade <mustafazadays16@gmail.com>
2026-03-28 16:06:51 +0000

    Print integer

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except SyntaxError:
-        print("False")
+        return True
+    except (TypeError, ValueError):
+        return False
