64194e178dfa14aad59d81615f813e40d74c5c81
AysuMustafazade <mustafazadays16@gmail.com>
2026-03-29 16:41:53 +0000

    Add integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module provides one function, add_integer(a, b).
+The function adds two integers and handles type validation.
+"""
 
 def add_integer(a, b=98):
     """Adds two integers.
