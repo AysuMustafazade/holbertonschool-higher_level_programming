6854659570db9bbadbd354b54907e9f62b404c0f
AysuMustafazade <mustafazadays16@gmail.com>
2026-03-29 17:28:12 +0000

    Print square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -3,7 +3,7 @@
 
 
 def print_square(size):
-"""
+    """
     Prints a square with the character #.
 
     Args:
@@ -12,7 +12,7 @@ def print_square(size):
     Raises:
         TypeError: if size is not an integer.
         ValueError: if size is less than 0.
-"""
+    """
     if not isinstance (size, int):
         raise TypeError("size must be an integer")
     if size < 0:
