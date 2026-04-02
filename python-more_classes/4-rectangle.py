fbc1fb42d06ecfa1d91bae8d87bd4f04a1997b24
AysuMustafazade <mustafazadays16@gmail.com>
2026-04-02 12:50:42 +0000

    Rectangle

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -51,7 +51,7 @@ class Rectangle:
             print("")
         else:
             for i in range(self.__height):
-                print(str("#" * self.__width))
+                return str("#" * self.__width)
 
     def __repr__(self):
         print("Rectangle({}, {})".format(self.__width, self.__height))
