2b901bb34b810f3bf3cb4381b4f6c77d40d4b317
AysuMustafazade <mustafazadays16@gmail.com>
2026-04-02 13:15:08 +0000

    Rectangle

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -52,7 +52,7 @@ class Rectangle:
         """the rectangle with the character #"""
         if self.__height == 0 or self.__width == 0:
             return ""
-        return "\n".join([print_symbol * self.__width for i in range(self.__height)])
+        return "\n".join([Rectangle.print_symbol * self.__width for i in range(self.__height)])
 
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
