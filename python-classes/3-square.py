7669f87dc40492aef41e653d4a4ce903e5c97e95
AysuMustafazade <mustafazadays16@gmail.com>
2026-04-02 11:17:41 +0000

    Area of square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -10,6 +10,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """Defines an area."""
         area = self.__size**2
