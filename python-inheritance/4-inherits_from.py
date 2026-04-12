8e734c59efc7d625a9a4d0a47eb5bf72a3f0a363
AysuMustafazade <mustafazadays16@gmail.com>
2026-04-12 18:48:42 +0400

    inherits from

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,5 @@
 
 def inherits_from(obj, a_class):
     """inherits from the secified class"""
-    return isinstance(obj, a_class)
+    if type(obj) is a_class:
+        return isinstance(obj, a_class)
