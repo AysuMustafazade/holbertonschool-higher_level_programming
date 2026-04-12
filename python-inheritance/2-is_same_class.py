31f9a5c09f8d0f60c0b432842d7f229cd03ed13e
AysuMustafazade <mustafazadays16@gmail.com>
2026-04-12 18:39:09 +0400

    exactly an instance of the specified class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@
 
 def is_same_class(obj, a_class):
     """if the object is exactly an instance of the specified class"""
-    if isinstance(obj, a_class):
+    if isinstance(obj, a_class) and type(obj) == a_class:
         return True
     else:
         return False
