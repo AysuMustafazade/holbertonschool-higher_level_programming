38cdea97cf4e8e52a4a8662d4bc00dbfb60079e7
AysuMustafazade <mustafazadays16@gmail.com>
2026-05-03 21:05:52 +0400

    python-object_relational_mapping

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -11,7 +11,8 @@ from model_city import City
 
 if __name__ == "__main__":
     db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
-    engine = create_engine(db_url.format(sys.argv[1], sys.argv[2], sys.argv[3]),
+    engine = create_engine(db_url.format(sys.argv[1], sys.argv[2], 
+                                         sys.argv[3]),
                            pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
