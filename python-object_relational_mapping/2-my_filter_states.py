02502435fdb4afaf35a76de1885ba216cf116cff
AysuMustafazade <mustafazadays16@gmail.com>
2026-05-03 20:08:20 +0400

    python-object_relational_mapping

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
 
     query = "SELECT * FROM states WHERE name LIKE BINARY '{}' \
              ORDER BY states.id ASC".format(state_searched)
-    
+
     cursor.execute(query)
 
     rows = cursor.fetchall()
