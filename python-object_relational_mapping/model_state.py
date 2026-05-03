fbc9d77501abd6a3babaab56aaf4eb477989e7e3
AysuMustafazade <mustafazadays16@gmail.com>
2026-05-03 20:37:32 +0400

    python-object_relational_mapping

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,6 +9,9 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    State class inherits from Base and links to the MySQL table 'states'
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
